48104b8b553f74ec484a281b06aaf133975da581
mkalia94 <manukalia94@gmail.com>
2020-07-31 11:42:29 +0200

    Final code

--- src/exec/autoencoder.jl
+++ src/exec/autoencoder.jl
@@ -84,8 +84,9 @@ function get_autoencoder(args::Dict)
     decoder = get_NN_Flux(reverse(AE_widths),[reverse(args["AE_acts"])[2:end];"id"])
     par_encoder = get_NN_Flux(Par_widths,Par_acts)
     par_decoder = get_NN_Flux(reverse(Par_widths),[reverse(args["Par_acts"])[2:end];"id"])
-
-    return encoder, decoder, par_encoder,par_decoder
+    u0_train = rand(Float32,args["z_dim"],args["BatchSize"])
+ 
+    return encoder, decoder, par_encoder,par_decoder, u0_train
 end
 
 
@@ -162,11 +163,12 @@ function hess_vec(args,NN,input_,acts, xb,xt)
     tdim = args["tsize"]
     n_ = args["x_dim"]
     p_ = args["par_dim"]
-    dxx = reshape(0.0f0 .* xb, n_, tdim*p_)
+    dxx_temp = reshape(xb, args["x_dim"],tdim*p_)
+    dxx = 0.0f0 .* NN(dxx_temp)
     for j = 1:n_
         for k = 1:n_
             id_4 = hcat([Matrix{Float32}(I,args["tsize"],args["tsize"]) for i in 1:p_]...) |> gpu
-            dxidxj = d2t_NN(NN,input_,acts,j,k)*id_4 # (n,t x p)            
+            dxidxj = d2t_NN(NN,input_,acts,j,k)*id_4 # (n,t x p)
             prod_ = reshape(xb[k,:,:] .* xt[j,:],1,tdim*p_) # (1, t x p)
             dxx = dxx .+ dxidxj.*prod_ # (n, t x p)
         end
@@ -174,68 +176,110 @@ function hess_vec(args,NN,input_,acts, xb,xt)
     return dxx
 end
 
-function build_loss(args,dzdt_rhs,dzdt_sens_rhs,encoder, decoder, par_encoder,par_decoder)    
-    function loss_(in_,dx_,par_,dxda,dtdxda)
+function build_loss(args,dzdt_rhs,dzdt_sens_rhs,encoder, decoder, par_encoder,par_decoder,u0_train)    
+    t_batch = range(0.0f0,Float32(args["tspan"][1]),length = args["tsize"])
+    ode_prob_temp = ODEProblem(dzdt_rhs,rand(Float32,args["z_dim"]),(0.0f0,Float32(args["tspan"][2])),gpu(rand(args["par_dim"])))
+    solve(ode_prob_temp)
+    ode_prob = (x,y) -> remake(ode_prob_temp,p=x,u0=y)
+    #solve(ode_prob(gpu(rand(1))))
+    #ODE solve to be used for training
+    function predict_ODE_solve(x,y)
+        return Array(solve(ode_prob(x,y),Tsit5(),saveat=t_batch,reltol=1e-4)) 
+    end
+    function loss_(in_batch,dx_batch,par_batch,dxda_batch,dtdxda_batch)
         tdim = args["tsize"]
         n_ = args["x_dim"]
         p_ = args["par_dim"]
+        loss_AE_state = 0.0f0
+        loss_dxdt = 0.0f0
+        loss_dzdt = 0.0f0
+        loss_AE_par = 0.0f0
+        loss_sens_dtdzdb = 0.0f0
+        loss_sens_x = 0.0f0
+        loss_par_id = 0.0f0
+        loss_NLRAN_in = 0.0f0
+        loss_NLRAN_out = 0.0f0
+        loss_u0 = 0.0f0
+        
         # AE loss
-        enc_ = encoder(in_)
-        enc_par = par_encoder(par_)
-        dec_par = par_decoder(enc_par)
-        dec_ = decoder(enc_)
-        # Consistency losses
-        dz1 = dt_NN(encoder,in_,dx_,args["AE_acts"])
-        #enc_par_temp = enc_par*reduce(hcat,[Matrix{Float32}(I,1,1) for i in 1:tdim])
-        dx1 = dt_NN(decoder,enc_,dzdt_rhs(enc_,enc_par,0.0f0),[reverse(args["AE_acts"])[2:end];"id"])
-        # Sensitivity losses
-        # 1. Compute dxdb and dtdxdb
-        dxda_ = reshape(dxda,n_*tdim,p_)
-        dtdxda_ = reshape(dtdxda,n_*tdim,p_)
-        id_ = hcat([Matrix{Float32}(I,1,1) for i in 1:p_]...) |> gpu
-        p_temp = enc_par*id_
-        id_p = Matrix{Float32}(I,p_,p_) |> gpu
-        dxdb_ = dxda_*dt_NN(par_decoder,p_temp,id_p,[reverse(args["Par_acts"])[2:end];"id"])
-        dxdb = reshape(dxdb_,n_,tdim,p_)
-        dtdxdb_ = dtdxda_*dt_NN(par_decoder,p_temp,id_p,[reverse(args["Par_acts"])[2:end];"id"])
-        dtdxdb = reshape(dtdxdb_,n_,tdim,p_)
-        # 2. dzdb and dtdzdb
-        dxdb_ = reshape(dxdb,n_,tdim*p_)
-        dtdxdb_ = reshape(dtdxdb,n_,tdim*p_)
-        id2_ = hcat([Matrix{Float32}(I,args["tsize"],args["tsize"]) for i in 1:p_]...) |> gpu
-        x_temp = in_*id2_
-        dzdb_ = dt_NN(encoder,x_temp,dxdb_,args["AE_acts"])
-        dtdzdb_2 = dt_NN(encoder,x_temp,dtdxdb_,args["AE_acts"])
-        dtdzdb_1 = hess_vec(args,encoder,in_,args["AE_acts"],dxdb,dx_)
-        dtdzdb_ = dtdzdb_1 .+ dtdzdb_2
-        dtdzdb = reshape(dtdzdb_,args["z_dim"],tdim,p_)
-        dzdb = reshape(dzdb_,args["z_dim"],tdim,p_)
-        # 3. Compute dtdzdb_data
-        dtdzdb_data_ = vcat([dzdt_sens_rhs(enc_[:,i],enc_par,0.0f0,dzdb[:,i,:]) for i in 1:tdim]...) |> gpu
-        dtdzdb_data = reshape(dtdzdb_data_,args["z_dim"],tdim,p_)
-        # 4. Compute dxda_decode
-        dxda_ = reshape(dxda,n_,tdim*p_)
-        id_3 = hcat([Matrix{Float32}(I,args["tsize"],args["tsize"]) for i in 1:p_]...) |> gpu
-        z_temp = enc_*id_3
-        dxda_decode_1 = dt_NN(encoder, x_temp, dxda_, args["AE_acts"])
-        dxda_decode_ = dt_NN(decoder, z_temp, dxda_decode_1, [reverse(args["AE_acts"])[2:end];"id"] )
-        dxda_decode = reshape(dxda_decode_,n_,tdim,p_)
-        
-        
-        loss_AE_state = args["P_AE_state"]*Flux.mse(in_,dec_)
-        loss_dxdt = args["P_cons_x"]*Flux.mse(dx_,dx1)
-        loss_dzdt = args["P_cons_z"]*Flux.mse(dz1,dzdt_rhs(enc_,enc_par,0.0f0))
-        loss_AE_par = args["P_AE_par"]*Flux.mse(dec_par,par_)
-        loss_sens_dtdzdb = args["P_sens_dtdzdb"]*Flux.mse(dtdzdb,dtdzdb_data)
-        loss_sens_x = args["P_sens_x"]*Flux.mse(dxda,dxda_decode)
-        
+        for i in args["BatchSize"]
+
+            in_ = in_batch[:,:,i]
+            dx_ = dx_batch[:,:,i]
+            par_ = par_batch[:,i]
+            dxda = dxda_batch[:,:,:,i]
+            dtdxda = dtdxda_batch[:,:,:,i]
+            
+            enc_ = encoder(in_)
+            par_adjust = par_encoder(par_)
+            enc_par = par_adjust # CAREFUL: TRANSLATION ADDED
+            dec_par = par_decoder(par_adjust) # CAREFUL: TRANSLATION ADDED
+            dec_ = decoder(enc_)
+            # Consistency losses
+            dz1 = dt_NN(encoder,in_,dx_,args["AE_acts"])
+            #enc_par_temp = enc_par*reduce(hcat,[Matrix{Float32}(I,1,1) for i in 1:tdim])
+            dx1 = dt_NN(decoder,enc_,dzdt_rhs(enc_,enc_par,0.0f0),[reverse(args["AE_acts"])[2:end];"id"])
+            # NLRAN losses
+            enc_ODE_solve = predict_ODE_solve(enc_par,u0_train[:,i])
+            
+            
+            # Sensitivity losses
+            # 1. Compute dxdb and dtdxdb
+            # dxda_ = reshape(dxda,n_*tdim,p_)
+            # dtdxda_ = reshape(dtdxda,n_*tdim,p_)
+            # id_ = hcat([Matrix{Float32}(I,1,1) for i in 1:p_]...) |> gpu
+            # p_temp = enc_par*id_
+            # id_p = Matrix{Float32}(I,p_,p_) |> gpu
+            # dxdb_ = dxda_*dt_NN(par_decoder,p_temp,id_p,[reverse(args["Par_acts"])[2:end];"id"])
+            # dxdb = reshape(dxdb_,n_,tdim,p_)
+            # dtdxdb_ = dtdxda_*dt_NN(par_decoder,p_temp,id_p,[reverse(args["Par_acts"])[2:end];"id"])
+            # dtdxdb = reshape(dtdxdb_,n_,tdim,p_)
+            # # 2. dzdb and dtdzdb
+            # dxdb_ = reshape(dxdb,n_,tdim*p_)
+            # dtdxdb_ = reshape(dtdxdb,n_,tdim*p_)
+            # id2_ = hcat([Matrix{Float32}(I,args["tsize"],args["tsize"]) for i in 1:p_]...) |> gpu
+            # x_temp = in_*id2_
+            # dzdb_ = dt_NN(encoder,x_temp,dxdb_,args["AE_acts"])
+            # dtdzdb_2 = dt_NN(encoder,x_temp,dtdxdb_,args["AE_acts"])
+            # dtdzdb_1 = hess_vec(args,encoder,in_,args["AE_acts"],dxdb,dx_)
+            # dtdzdb_ = dtdzdb_1 .+ dtdzdb_2
+            # dtdzdb = reshape(dtdzdb_,args["z_dim"],tdim,p_)
+            # dzdb = reshape(dzdb_,args["z_dim"],tdim,p_)
+            # # 3. Compute dtdzdb_data
+            # dtdzdb_data_ = vcat([dzdt_sens_rhs(enc_[:,i],enc_par,0.0f0,dzdb[:,i,:]) for i in 1:tdim]...) |> gpu
+            # dtdzdb_data = reshape(dtdzdb_data_,args["z_dim"],tdim,p_)
+            # # 4. Compute dxda_decode
+            # dxda_ = reshape(dxda,n_,tdim*p_)
+            # id_3 = hcat([Matrix{Float32}(I,args["tsize"],args["tsize"]) for i in 1:p_]...) |> gpu
+            # z_temp = enc_*id_3
+            # dxda_decode_1 = dt_NN(encoder, x_temp, dxda_, args["AE_acts"])
+            # dxda_decode_ = dt_NN(decoder, z_temp, dxda_decode_1, [reverse(args["AE_acts"])[2:end];"id"] )
+            # dxda_decode = reshape(dxda_decode_,n_,tdim,p_)
+            
+            
+            loss_AE_state = loss_AE_state + Float32(1/args["BatchSize"])*args["P_AE_state"]*Flux.mse(in_,dec_)
+            loss_dxdt = loss_dxdt + Float32(1/args["BatchSize"])*args["P_cons_x"]*Flux.mse(dx_,dx1)
+            loss_dzdt = loss_dzdt + Float32(1/args["BatchSize"])*args["P_cons_z"]*Flux.mse(dz1,dzdt_rhs(enc_,enc_par,0.0f0))
+            loss_AE_par = loss_AE_par + Float32(1/args["BatchSize"])*args["P_AE_par"]*Flux.mse(dec_par,par_)
+            loss_NLRAN_in = loss_NLRAN_in + Float32(1/args["BatchSize"])*args["P_NLRAN_in"]*Flux.mse(enc_ODE_solve,enc_)
+            loss_NLRAN_out = loss_NLRAN_out + Float32(1/args["BatchSize"])*args["P_NLRAN_out"]*Flux.mse(in_,decoder(enc_ODE_solve))
+            loss_u0 = loss_u0 + Float32(1/args["BatchSize"])*args["P_u0"]*Flux.mse(u0_train[:,i],enc_[:,1])
+            # loss_sens_dtdzdb = loss_sens_dtdzdb + Float32(1/args["BatchSize"])*args["P_sens_dtdzdb"]*Flux.mse(dtdzdb,dtdzdb_data)
+            # loss_sens_x = loss_sens_x + Float32(1/args["BatchSize"])*args["P_sens_x"]*Flux.mse(dxda,dxda_decode)
+            # loss_par_id = loss_par_id + Float32(1/args["BatchSize"])*args["P_AE_id"]*Flux.mse(par_,par_adjust)
+        end
         args["loss_AE"] = loss_AE_state
         args["loss_dxdt"] = loss_dxdt
         args["loss_dzdt"] = loss_dzdt
         args["loss_par"] = loss_AE_par
         args["loss_sens_dt"] = loss_sens_dtdzdb
         args["loss_sens_x"] = loss_sens_x
-        loss_total = loss_AE_state + loss_dxdt + loss_dzdt + loss_AE_par + loss_sens_dtdzdb + loss_sens_x
+        args["loss_par_id"] = loss_par_id
+        args["loss_NLRAN_in"] = loss_NLRAN_in
+        args["loss_NLRAN_out"] = loss_NLRAN_out
+        args["loss_u0"] = loss_u0
+        
+        loss_total = loss_AE_state + loss_dxdt + loss_dzdt + loss_AE_par + loss_sens_dtdzdb + loss_sens_x + loss_par_id + loss_NLRAN_in + loss_NLRAN_out + loss_u0
         args["loss_total"] = loss_total
         return loss_total
     end
---
+++ src/problems/Brusselator_PDE.jl
@@ -0,0 +1,138 @@
+# x: Hopf NF + quartic nonlinearity
+# z: Hopf NF
+# par: scalar Hopf parameter; subcritical Hopf
+
+# dxdt_rhs
+function dxdt_rhs(dx_,u,par,t)
+    x = u .+ args["bif_x"]
+    p = par .+ args["bif_p"]
+    aa = args["par_aa"]
+    bb = args["par_bb"]
+    dx = args["par_dx"]
+    dy = args["par_dy"]
+    # x = 0
+    dx_[1] = 0.0f0
+    dx_[2] = 0.0f0
+    # x = N
+    dx_[end-1] = 0.0f0
+    dx_[end] = 0.0f0
+    i = 3
+    while i<(args["x_dim"]-2)
+        dx_[i] = (dx/p[1]*(x[i-2] + x[i+2] - 2.0f0 * x[i])*(args["x_dim"]-1)^2 + x[i]^2*x[i+1] - (bb + 1.0f0)*x[i] + aa)
+        dx_[i+1] = (dy/p[1]*(x[i-2] + x[i+2] -2.0f0 * x[i])*(args["x_dim"]-1)^2 + bb*x[i-1] - x[i-1]^2*x[i])
+        i = i+2
+    end
+    return dx
+end
+
+function dxdt_rhs(u,par,t)
+    x = u .+ args["bif_x"]
+    p = par .+ args["bif_p"]
+    dx_ = Zygote.Buffer(x,args["x_dim"],args["tsize"])
+    ind_ = args["x_dim"]
+    aa = args["par_aa"]
+    bb = args["par_bb"]
+    dx = args["par_dx"]
+    dy = args["par_dy"]
+    # x = 0
+    dx_[1,:] = zeros(Float32,args["tsize"])
+    dx_[2,:] = zeros(Float32,args["tsize"])
+    # x = N
+    dx_[ind_-1,:] = zeros(Float32,args["tsize"])
+    dx_[ind_,:] = zeros(Float32,args["tsize"])
+    i = 3
+    while i<(args["x_dim"]-2)
+        dx_[i,:] = dx/p[1] .* (x[i-2,:] .+ x[i+2,:] .- 2.0f0 .* x[i,:]) .* (args["x_dim"]-1)^2 .+ (x[i,:] .^ 2.0f0) .* x[i+1,:] .- (bb + 1.0f0) .* x[i,:] .+ aa
+        dx_[i+1,:] = dy/p[1] .* (x[i-2,:] .+ x[i+2,:] .- 2.0f0 .* x[i,:]) .* (args["x_dim"]-1)^2 .+ bb .* x[i-1,:] .- (x[i-1,:].^2.0f0) .* x[i,:]
+        i = i+2
+    end
+    return copy(dx_)
+end
+
+# dxdt_jac 
+function dxdt_jac(u,par,t)
+    x = u .+ args["bif_x"]
+    p = par .+ args["bif_p"]
+    dx_ = Zygote.Buffer(x,args["x_dim"],args["x_dim"])
+    ind_ = args["x_dim"]
+    dx_[1,1] = -1.0f0
+    dx_[1,2] = x[ind_]
+    dx_[1,3:ind_-2] = zeros(Float32,ind_-4)
+    dx_[1,ind_-1] = - x[ind_]
+    dx_[1,ind_] = x[2]
+    # ------------------
+    dx_[2,1] = x[3]
+    dx_[2,2] = -1.0f0
+    dx_[2,3] = x[1]
+    dx_[2,4:ind_-1] = zeros(Float32,ind_-4)
+    dx_[2,ind_] = -x[1]
+    # -----------------
+    for i=3:(size(dx_,1)-1)
+        if i != 3
+            dx_[i,1:i-3] = zeros(Float32,i-3)
+        end
+        dx_[i,i-2] = -x[i-1]
+        dx_[i,i-1] = x[i+1]
+        dx_[i,i] = -1.0f0
+        dx_[i,i+1] = x[i-1]
+        if i != args["x_dim"]
+            dx_[i,i+2:ind_] = zeros(Float32,ind_-i-1)
+        end
+    end
+    # ----------------
+    dx_[ind_,1] = x[ind_-1]
+    dx_[ind_,2:ind_-3] = zeros(Float32,ind_-4)
+    dx_[ind_,ind_-2] = -x[ind_-1]
+    dx_[ind_,ind_-1] = x[1]
+    dx_[ind_,ind_] = -1.0f0
+    return copy(dx_)
+end
+
+function dxdt_sens(u,par,t)
+    x = u .+ args["bif_x"]
+    p = par .+ args["bif_p"]
+    dx_ = Zygote.Buffer(x,args["x_dim"],args["par_dim"])
+    dx_[1:args["x_dim"],1] = zeros(Float32,args["x_dim"]) .+ 1.0f0
+    return copy(dx_)
+end
+
+function dxdt_sens_rhs(x,p,t,dxda)
+    return dxdt_jac(x,p,t)*dxda .+ dxdt_sens(x,p,t)
+end
+
+# ------------------------------------------------------------------------------------
+
+# dzdt_rhs
+function dzdt_rhs(dz,z,p,t)
+    dz[1] = p[1]*z[1]-z[2]-z[1]*(z[1]^2+z[2]^2)
+    dz[2] = z[1]+p[1]*z[2]-z[2]*(z[1]^2+z[2]^2)
+    return dz
+end
+
+function dzdt_rhs(x,p,t)
+    dx_ = Zygote.Buffer(x,args["z_dim"],args["tsize"])
+    dx_[1,:] = p[1].*x[1,:] .- x[2,:] .- x[1,:] .* (x[1,:].^2 .+ x[2,:].^2)
+    dx_[2,:] = x[1,:] .+ p[1] .* x[2,:] .- x[2,:] .*(x[1,:].^2 .+ x[2,:].^2)
+    return copy(dx_)
+end
+
+# dzdt_jac 
+function dzdt_jac(x,p,t)
+    dx_ = Zygote.Buffer(x,args["z_dim"],args["z_dim"])
+    dx_[1,1] = p[1]-(3.0f0*x[1]^2+x[2]^2)
+    dx_[1,2] = -1.0f0-x[1]*(2.0f0*x[2])
+    dx_[2,1] = 1.0f0-x[2]*(2.0f0*x[1])
+    dx_[2,2] = p[1]-(x[1]^2+3.0f0*x[2]^2)
+    return copy(dx_)
+end
+
+function dzdt_sens(x,p,t)
+    dx_ = Zygote.Buffer(x,args["z_dim"],args["par_dim"])
+    dx_[1,1] = x[1]
+    dx_[2,1] = x[2]
+    return copy(dx_)
+end
+
+function dzdt_sens_rhs(x,p,t,dxda)
+    return dzdt_jac(x,p,t)*dxda .+ dzdt_sens(x,p,t)
+end 
